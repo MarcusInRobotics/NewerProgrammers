db6e46ed709c8e0b8f21abb14a31f31beca4ec83
mabm5 <mabm5@k12albemarle.org>
2025-03-01 22:12:47 -0500

    HOWDY HOWDY WAHS PROGRAMMERS

--- TeamCode/src/main/java/teamcode/ForNewProgrammers/SampleAutonomous.java
+++ TeamCode/src/main/java/teamcode/ForNewProgrammers/SampleAutonomous.java
@@ -1,5 +1,12 @@
 package teamcode.ForNewProgrammers;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 public class SampleAutonomous {
+
+    public void test (Gamepad gamepad) {
+
+    }
+
 
 }
